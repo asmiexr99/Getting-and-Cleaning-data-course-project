151934c031aa6153c6b618fd0333e2fb7b5bbc7f
asmiexr99 <asmerom.r.tesfamichael@hpe.com>
2015-11-13 21:10:58 -0600

    Update run analysis.R

--- run analysis.R
+++ run analysis.R
@@ -1,88 +1,80 @@
 # Getting-and-Cleaning-data-course-project
-require(plyr)
 
-# Directories and loading
-data <- "UCI\ HAR\ Dataset"
-Feature <- paste(data, "/features.txt", sep = "")
-activity_labels_data <- paste(data, "/activity_labels.txt", sep = "")
-x_train_file <- paste(data, "/train/X_train.txt", sep = "")
-y_train_file <- paste(data, "/train/y_train.txt", sep = "")
-subject_train_file <- paste(data, "/train/subject_train.txt", sep = "")
-x_test_file  <- paste(data, "/test/X_test.txt", sep = "")
-y_test_file  <- paste(data, "/test/y_test.txt", sep = "")
-subject_test_file <- paste(data, "/test/subject_test.txt", sep = "")
-Features <- read.table(Feature, colClasses = c("character"))
-activity_labels <- read.table(activity_labels_file, col.names = c("ActivityId", "Activity"))
-x_train <- read.table(x_train_file)
-y_train <- read.table(y_train_file)
-subject_train <- read.table(subject_train_file)
-x_test <- read.table(x_test_file)
-y_test <- read.table(y_test_file)
-subject_test <- read.table(subject_test_file)
+
+if (!getwd() == "./out-of-box-samples") {
+    dir.create("./out-of-box-samples")
+}
+
+rm(list = ls(all = TRUE))
+library(plyr) 
+library(data.table) 
+library(dplyr) 
+
+get <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",get)
+unzip(get, list = TRUE) 
+YTest <- read.table(unzip(get, "UCI HAR Dataset/test/y_test.txt"))
+XTest <- read.table(unzip(get, "UCI HAR Dataset/test/X_test.txt"))
+SubjectTest <- read.table(unzip(get, "UCI HAR Dataset/test/subject_test.txt"))
+YTrain <- read.table(unzip(get, "UCI HAR Dataset/train/y_train.txt"))
+XTrain <- read.table(unzip(get, "UCI HAR Dataset/train/X_train.txt"))
+SubjectTrain <- read.table(unzip(get, "UCI HAR Dataset/train/subject_train.txt"))
+Features <- read.table(unzip(get, "UCI HAR Dataset/features.txt"))
+unlink(get) 
+
+colnames(XTrain) <- t(Features[2])
+colnames(XTest) <- t(Features[2])
+
+XTrain$activities <- YTrain[, 1]
+XTrain$participants <- SubjectTrain[, 1]
+XTest$activities <- YTest[, 1]
+XTest$participants <- SubjectTest[, 1]
 
 ##################################################################
 # 1. Merges the training and the test sets to create one data set.
 ##################################################################
-training_data <- cbind(cbind(x_train, subject_train), y_train)
-test_data <- cbind(cbind(x_test, subject_test), y_test)
-all_data <- rbind(training_data, test_data)
-sensor_labels <- rbind(rbind(Features, c(562, "Subject")), c(563, "ActivityId"))[,2]
-colNames  = colnames(all_data); 
+all_data <- rbind(XTrain, XTest)
+duplicated(colnames(all_data))
+all_data <- all_data[, !duplicated(colnames(all_data))]
 
 ############################################################################################
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 ############################################################################################
+data_mean <- grep("mean()", names(all_data), value = FALSE, fixed = TRUE)
+dat_mean <- append(data_mean, 471:477)
+MeanMatrix <- all_data[data_mean]
+data_std <- grep("std()", names(all_data), value = FALSE)
+STDMatrix <- all_data[data_std]
 
-data_mean_std <- (grepl("activity..",colNames) | grepl("subject..",colNames) | grepl("-mean..",colNames) & !grepl("-meanFreq..",colNames) & !grepl("mean..-",colNames) | grepl("-std..",colNames) & !grepl("-std()..-",colNames));
-all_data = all_data[data_mean_std==TRUE];
 ###########################################################################
 # 3. Uses descriptive activity names to name the activities in the data set
 ###########################################################################
-all_data = merge(all_data,activityType,by='activityId',all.x=TRUE);
-colNames  = colnames(all_data); 
+all_data$activities <- as.character(all_data$activities)
+all_data$activities[all_data$activities == 1] <- "Walking"
+all_data$activities[all_data$activities == 2] <- "Walking Upstairs"
+all_data$activities[all_data$activities == 3] <- "Walking Downstairs"
+all_data$activities[all_data$activities == 4] <- "Sitting"
+all_data$activities[all_data$activities == 5] <- "Standing"
+all_data$activities[all_data$activities == 6] <- "Laying"
+all_data$activities <- as.factor(all_data$activities)
 
-data_mean_std <- join(data_mean_std, activity_labels, by = "ActivityId", match = "first")
-data_mean_std <- data_mean_std[,-1]
-colNames  = colnames(data_mean_std); 
 ##############################################################
 # 4. Appropriately labels the data set with descriptive names.
 ##############################################################
-for (i in 1:length(colNames)) 
-{
-  colNames[i] = gsub("\\()","",colNames[i])
-  colNames[i] = gsub("-std$","StdDev",colNames[i])
-  colNames[i] = gsub("-mean","Mean",colNames[i])
-  colNames[i] = gsub("^(t)","time",colNames[i])
-  colNames[i] = gsub("^(f)","freq",colNames[i])
-  colNames[i] = gsub("([Gg]ravity)","Gravity",colNames[i])
-  colNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
-  colNames[i] = gsub("[Gg]yro","Gyro",colNames[i])
-  colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
-  colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
-  colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
-  colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
-};
-colnames(data_mean_std) = colNames;
-##############################################################
-# 4. Appropriately labels the data set with descriptive names.
-##############################################################
-
-names(data_mean_std) <- make.names(names(data_mean_std))
-names(data_mean_std) <- gsub('Acc',"Acceleration",names(data_mean_std))
-names(data_mean_std) <- gsub('GyroJerk',"AngularAcceleration",names(data_mean_std))
-names(data_mean_std) <- gsub('Gyro',"AngularSpeed",names(data_mean_std))
-names(data_mean_std) <- gsub('Mag',"Magnitude",names(data_mean_std))
-names(data_mean_std) <- gsub('^t',"TimeDomain.",names(data_mean_std))
-names(data_mean_std) <- gsub('^f',"FrequencyDomain.",names(data_mean_std))
-names(data_mean_std) <- gsub('\\.mean',".Mean",names(data_mean_std))
-names(data_mean_std) <- gsub('\\.std',".StandardDeviation",names(data_mean_std))
-names(data_mean_std) <- gsub('Freq\\.',"Frequency.",names(data_mean_std))
-names(data_mean_std) <- gsub('Freq$',"Frequency",names(data_mean_std))
+names(all_data) <- gsub("Acc", "Accelerator", names(all_data))
+names(all_data) <- gsub("Mag", "Magnitude", names(all_data))
+names(all_data) <- gsub("Gyro", "Gyroscope", names(all_data))
+names(all_data) <- gsub("^t", "time", names(all_data))
+names(all_data) <- gsub("^f", "frequency", names(all_data))
+names(all_data) <- gsub("-std$", "StdDev", names(all_data))
+names(all_data) <- gsub("-mean", "Mean", names(all_data))
 
 ######################################################################################################################
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 ######################################################################################################################
-tidy_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
-write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
+all_data_master <- data.table(all_data)
+#This takes the mean of every column broken down by participants and activities
+tidy_data <- all_data_master[, lapply(.SD, mean), by = 'participants,activities']
+write.table(tidy_data, file = "tidy_data.txt", row.names = FALSE)
 
 
